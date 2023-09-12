85d18be6f5995c72e42260d2633c641647cc186c
Nico <89244947+nicovalerian@users.noreply.github.com>
2023-09-12 18:18:57 +0700

    Create round win and round lost announcement

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ function mainGame() {
     let playerPrompt = prompt("Pick! Rock, paper, or scissors?");
     let playerSelection = playerPrompt.toLowerCase();
     let computerSelection = getComputerChoice();
+
+    let announceRoundLose = `You Lose! ${computerSelection} beats ${playerSelection}!`;
+    let announceRoundWin = `You Win! ${playerSelection} beats ${computerSelection}!`;
+    
+
+    if ((computerSelection == "Rock") && (playerSelection != "Paper")) {
+        return 
+    }
 }
-
-
